4a4cae7951e3739161da248ab4c92cd14c5212b7
Richard Millet <richard.millet@berkeley.edu>
2014-07-24 16:23:27 -0700

    Raising a 404 error.

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,6 @@ import views
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'^(?P<poll_id>\d+)/$', views.detail, name='detail'),
-                       url(r'(?P<poll_id>\d+)/results/$', views.results, name='results'),
-                       url(r'(?P<poll_id>\d+)/vote/$', views.vote, name='results'),
+                       url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
+                       url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
                        )
--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 from polls.models import Poll
 
 # Create your views here.
 
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    return HttpResponse("Hello, world.  You're at the poll index.")
+
+    # template = loader.get_template('polls/index.html')
+    # context = RequestContext(request, {'latest_poll_list': latest_poll_list})
+    # return HttpResponse(template.render(context))
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at the details for poll %s" % poll_id)
+    poll = Poll.objects.get(pk=poll_id)
+    poll_choice_list = poll.choice_set.all()
+    context = {'poll': poll, 'poll_choice_list': poll_choice_list}
+    return render(request, 'polls/detail.html', context)
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s" % poll_id)
